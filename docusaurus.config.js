c9a174285d2a12d957a8062233cd04054589279d
Jose J. Cruz <jjzcru@gmail.com>
2022-12-17 20:12:02 -0500

    fix: :bug: Change github repository link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -152,7 +152,7 @@ const config = {
 							},
 							{
 								label: 'GitHub',
-								href: 'https://github.com/facebook/docusaurus',
+								href: 'https://github.com/jjzcru/clean-architecture-docusaurus',
 							},
 						],
 					},
